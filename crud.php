0b0577e173e482b0926a2fc4d42700e73b20b3b0
André Bazaglia <andre@bazaglia.com>
2015-10-07 13:36:31 -0300

    Função 'inserir' agora usa querys preparadas.

--- crud.php
+++ crud.php
@@ -4,26 +4,29 @@
         private $user = "root";
         private $senha = "";
         private $pdo;
-        
+
         function __construct() {
         }
-        
+
         private function conectar(){
             if(empty($this->pdo)){
                 $this->pdo = new PDO($this->dsn, $this->user, $this->senha);
                 return $this->pdo;
             }
         }
-    
+
         public function inserir(array $campos, $tabela){
+            $pdo = $this->conectar();
             $coluna = implode(", ",  array_keys($campos));
             $valor = "'".implode("', '", array_values($campos))."'";
-            
-            $query = "INSERT INTO {$tabela} ({$coluna}) VALUES ({$valor})";
-            
-            $this->conectar()->exec($query);
+
+            $query = $pdo->prepare("INSERT INTO :tabela (:coluna) VALUES (:valor)");
+            $query->bindParam(':tabela', $tabela, PDO::PARAM_STR);
+            $query->bindParam(':coluna', $coluna, PDO::PARAM_STR);
+            $query->bindParam(':valor', $valor, PDO::PARAM_STR);
+            $query->execute();
         }
-        
+
         public function consultar(array $select, $tabela, $where = null, $order = null, $limit = null){
             $where = ($where == null) ? null : "WHERE {$where}";
             if($select != "*"){
@@ -31,36 +34,36 @@
             } else {
                 $select = "*";
             };
-            
+
             $order = ($order == null) ? null : "ORDER BY {$order}";
             $limit = ($limit == null) ? null : "LIMIT {$limit}";
-            
+
             $query = "SELECT {$select} FROM {$tabela} {$where} {$order} {$limit}";
             $result = $this->conectar()->query($query);
             //echo "<strong>Query do método consulta():</strong><br /> <code style='color:green'>".$query."</code><br /><br />";
             return $result;
         }
-        
+
         public function deletar($tabela, $where){
             $query = "DELETE FROM {$tabela} WHERE {$where}";
-            
+
             $this->conectar()->exec($query);
             //echo "<strong>Query do método deletar():</strong><br /> <code style='color:green'>".$query."</code><br /><br />";
         }
-        
+
         public function atualizar($tabela, array $set, $where){
             foreach($set as $chave => $valor){
                 $campos[] = "{$chave}='{$valor}'";
             };
             $campos = implode(", ", $campos);
             $query = "UPDATE {$tabela} SET {$campos} WHERE {$where}";
-            
+
             $result = $this->conectar()->exec($query);
-            
+
             //if($result == 1){
             //    echo "Registro Atualizado com êxito";
             //};
-            
+
             //echo "<strong>Query do método atualizar():</strong><br /> <code style='color:green'>".$query."</code><br /><br />";
         }
 }
